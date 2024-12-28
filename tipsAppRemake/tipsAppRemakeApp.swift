a79c88aa1c95eade0ef0b2ca4f6db6215a4d4a22
Justin Hold <36420615+jhold1982@users.noreply.github.com>
2024-12-28 11:37:54 -0600

    Initial Commit

---
+++ tipsAppRemake/tipsAppRemakeApp.swift
@@ -0,0 +1,17 @@
+//
+//  tipsAppRemakeApp.swift
+//  tipsAppRemake
+//
+//  Created by Justin Hold on 12/28/24.
+//
+
+import SwiftUI
+
+@main
+struct tipsAppRemakeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
